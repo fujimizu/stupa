7a128bcdc907e82ddac71eaed85cba9ab4ad2d37
mfujisa <mfujisa@7e4e6e64-0ae8-11df-a48f-3fa99c96b54c>
2010-01-27 10:21:07 +0000

    [trunk] modify bsrand, bsmgr

--- stupa/bsmgr.cc
+++ stupa/bsmgr.cc
@@ -59,10 +59,10 @@ int main(int argc, char **argv) {
  */
 static void usage(const char *progname) {
   fprintf(stderr, "%s: Bayesian Sets utility\n\n", progname);
-  fprintf(stderr, "Usage\n");
-  fprintf(stderr, " %% %s search file [invsize]   (default invsize=%d)\n",
+  fprintf(stderr, "Usage:\n");
+  fprintf(stderr, " %% %s search file [invsize]  (default_invsize=%d)\n",
           progname, static_cast<int>(DEFAULT_INV_SIZE));
-  fprintf(stderr, " %% %s save infile outfile invsize\n", progname);
+  fprintf(stderr, " %% %s save infile outfile [invsize]\n", progname);
 }
 
 /**
@@ -83,13 +83,17 @@ static int run_search(int argc, char **argv) {
   printf("Read input documents ...\n");
   size_t invsize = argc == 4 ?
     static_cast<size_t>(atoi(argv[3])) : DEFAULT_INV_SIZE;
+  if (invsize <= 0) {
+    fprintf(stderr, "[ERROR]Size of inverted index must be greater than zero.");
+    return EXIT_FAILURE;
+  }
+
   stupa::BayesianSetsSearch bssearch(invsize);
   if (stupa::get_extension(argv[2]) == "tsv") {
     read_tsv(ifs, bssearch);
   } else {
     bssearch.load(ifs);
   }
-
   std::vector<std::string> queries;
   std::vector<std::pair<std::string, stupa::Point> > results;
   std::string line;
@@ -114,7 +118,7 @@ static int run_search(int argc, char **argv) {
  * @param argv argument strings
  */
 static int run_save(int argc, char **argv) {
-  if (argc < 5) {
+  if (argc < 4) {
     usage(argv[0]);
     return EXIT_FAILURE;
   }
@@ -128,7 +132,8 @@ static int run_save(int argc, char **argv) {
     fprintf(stderr, "[ERROR]Cannot open file: %s\n", argv[3]);
     return EXIT_FAILURE;
   }
-  int invsize = atoi(argv[4]);
+  size_t invsize = argc == 5 ?
+    static_cast<size_t>(atoi(argv[4])) : DEFAULT_INV_SIZE;
   if (invsize <= 0) {
     fprintf(stderr, "[ERROR]Size of inverted index must be greater than zero.");
     return EXIT_FAILURE;
