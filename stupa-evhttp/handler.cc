b98515e3ac86aeb81848ad275e196de016d45b0f
Mizuki Fujisawa <fujisawa@bayon.cc>
2010-04-21 10:49:20 +0000

    [trunk] add evhttp server

---
+++ stupa-evhttp/handler.cc
@@ -0,0 +1,26 @@
+//
+// Event handler
+//
+// Copyright(C) 2010  Mizuki Fujisawa <fujisawa@bayon.cc>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; version 2 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+
+#include "handler.h"
+
+namespace stupa {
+
+/* no implementaion */
+
+} /* namespace stupa */
