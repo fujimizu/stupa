97fcf2cd373cb9dea2b12b5e89edffcba1bb67a6
mfujisa <mfujisa@7e4e6e64-0ae8-11df-a48f-3fa99c96b54c>
2010-04-26 06:01:59 +0000

    [stupa-evhttp] modify perl client

--- stupa-evhttp/perl/StupaEvhttpClient/lib/StupaEvhttpClient.pm
+++ stupa-evhttp/perl/StupaEvhttpClient/lib/StupaEvhttpClient.pm
@@ -9,7 +9,7 @@ use WWW::Curl::Easy;
 
 our $VERSION = '0.01';
 
-__PACKAGE__->mk_accessors(qw(host port timeout));
+__PACKAGE__->mk_accessors(qw(curl host port timeout));
 
 use constant {
     TIMEOUT => 5,
@@ -18,10 +18,15 @@ use constant {
 sub new {
     my ($self, %argv) = @_;
     map { exists $argv{$_} or croak $_ } qw(host port);
+
+    my $timeout = $argv{timeout} || TIMEOUT;
+    my $curl = WWW::Curl::Easy->new;
+    $curl->setopt(CURLOPT_TIMEOUT, $timeout);
+    $curl->setopt(CURLOPT_HTTPHEADER, ['Connection: Keep-Alive']);
     $self->SUPER::new({
-        host       => $argv{host},
-        port       => $argv{port},
-        timeout    => $argv{timeout} || TIMEOUT,
+        host => $argv{host},
+        port => $argv{port},
+        curl => $curl,
     });
 }
 
@@ -36,48 +41,60 @@ sub _make_request_url {
     return $url;
 }
 
+sub _send_request {
+    my ($self, $url) = @_;
+    return if !$url;
+    $self->curl->setopt(CURLOPT_URL, $url);
+    open my $content, '>', \my $content_data;
+    $self->curl->setopt(CURLOPT_WRITEDATA, $content);
+    my $ret = $self->curl->perform();
+#    close $content;
+    return ($ret, $content_data);
+}
+
 sub add_document {
+    my ($self, $document_id, $feature_ids) = @_;
+    return if !defined $document_id && !$feature_ids;
+    my %option = (
+        id      => $document_id,
+        feature => join "\t", @{ $feature_ids },
+    );
+    my $url = $self->_make_request_url('add', \%option);
+    my ($ret, $response) = $self->_send_request($url);
+    return $ret == 0 ? 1 : 0;
 }
 
 sub delete_document {
+    my ($self, $document_id) = @_;
+    return if !defined $document_id;
+    my %option = (
+        id => $document_id
+    );
+    my $url = $self->_make_request_url('delete', \%option);
+    my ($ret, $response) = $self->_send_request($url);
+    return $ret == 0 ? 1 : 0;
 }
 
 sub size {
     my $self = shift;
-    my $curl = WWW::Curl::Easy->new;
     my $url = $self->_make_request_url('size');
-    $curl->setopt(CURLOPT_URL, $url);
-    $curl->setopt(CURLOPT_TIMEOUT, $self->timeout);
-    $curl->setopt(CURLOPT_HTTPHEADER, ['Connection: Keep-Alive']);
-
-    open my $content, '>', \my $content_data;
-    $curl->setopt(CURLOPT_WRITEDATA, $content);
-    my $ret = $curl->perform();
-    return if $ret != 0;
-    my $size = (split /\n/, $content_data)[0];
+    my ($ret, $response) = $self->_send_request($url);
+    return if $ret != 0 || !$response;
+    my $size = (split /\n/, $response)[0];
     return $size;
 }
 
 sub search_by_document {
     my ($self, $document_ids, $max) = @_;
     return if !$document_ids;
-
-    my $curl = WWW::Curl::Easy->new;
     my %option;
     $option{query} = join "\t", @{ $document_ids };
     $option{max} = $max if $max > 0;
     my $url = $self->_make_request_url('dsearch', \%option);
-    $curl->setopt(CURLOPT_URL, $url);
-    $curl->setopt(CURLOPT_TIMEOUT, $self->timeout);
-    $curl->setopt(CURLOPT_HTTPHEADER, ['Connection: Keep-Alive']);
-
-    open my $content, '>', \my $content_data;
-    $curl->setopt(CURLOPT_WRITEDATA, $content);
-    my $ret = $curl->perform();
-    return if $ret != 0;
-
+    my ($ret, $response) = $self->_send_request($url);
+    return if $ret != 0 || !$response;
     my %results;
-    foreach my $line (split /\n/, $content_data) {
+    foreach my $line (split /\n/, $response) {
         next if !$line;
         my ($document_id, $point) = split /\t/, $line;
         $results{$document_id} = $point;
@@ -88,23 +105,14 @@ sub search_by_document {
 sub search_by_feature {
     my ($self, $feature_ids, $max) = @_;
     return if !$feature_ids;
-
-    my $curl = WWW::Curl::Easy->new;
     my %option;
     $option{query} = join "\t", @{ $feature_ids };
     $option{max} = $max if $max > 0;
     my $url = $self->_make_request_url('dsearch', \%option);
-    $curl->setopt(CURLOPT_URL, $url);
-    $curl->setopt(CURLOPT_TIMEOUT, $self->timeout);
-    $curl->setopt(CURLOPT_HTTPHEADER, ['Connection: Keep-Alive']);
-
-    open my $content, '>', \my $content_data;
-    $curl->setopt(CURLOPT_WRITEDATA, $content);
-    my $ret = $curl->perform();
-    return if $ret != 0;
-
+    my ($ret, $response) = $self->_send_request($url);
+    return if $ret != 0 || !$response;
     my %results;
-    foreach my $line (split /\n/, $content_data) {
+    foreach my $line (split /\n/, $response) {
         next if !$line;
         my ($document_id, $point) = split /\t/, $line;
         $results{$document_id} = $point;
@@ -113,9 +121,25 @@ sub search_by_feature {
 }
 
 sub save {
+    my ($self, $filename) = @_;
+    return if !$filename;
+    my %option = (
+        file => $filename,
+    );
+    my $url = $self->_make_request_url('save', \%option);
+    my ($ret, $response) = $self->_send_request($url);
+    return $ret == 0 ? 1 : 0;
 }
 
 sub load {
+    my ($self, $filename) = @_;
+    return if !$filename;
+    my %option = (
+        file => $filename,
+    );
+    my $url = $self->_make_request_url('load', \%option);
+    my ($ret, $response) = $self->_send_request($url);
+    return $ret == 0 ? 1 : 0;
 }
 
 1;
@@ -124,15 +148,91 @@ __END__
 
 =head1 NAME
 
-StupaEvhttpClient -
+StupaEvhttpClient - client library of Stupa search engine
 
 =head1 SYNOPSIS
 
   use StupaEvhttpClient;
+  my $client = StupaEvhttpClient->new(
+      host    => 'localhost',
+      port    => 8080,
+      timeout => 5,
+  );
+  ## get size
+  my $size = $client->size();
+  
+  ## search by document ids
+  my $max = 10;
+  my @query = ('apple', 'banana');
+  my $results = $client->search_by_document(\@query, $max);
+  if ($results) {
+      my @document_ids = sort { $results->{$b} <=> $results->{$a} }
+          keys %{ $results };
+      for (my $i = 0; $i < scalar @document_ids; $i++) {
+          printf "%d\t%s\t%.3f\n",
+              $i+1, $document_ids[$i], $results->{$document_ids[$i]};
+      }
+  }
+  
+  ## delete a document
+  my $document_id = 'apple';
+  my $ret = $client->delete_document($document_id);
 
 =head1 DESCRIPTION
 
-StupaEvhttpClient is
+StupaEvhttpClient is a client library of Stupa search engine.
+
+=head1 METHODS
+
+=head2 new(%arg)
+
+Create a new instance.
+
+'host' option is the host name of Stupa server, and must be specified.
+
+'port' option is the port number of Stupa server, and must be specified.
+
+'timeout' option is the timeout seconds of the connection to Stupa server, and default value is 5.
+
+=head2 add_document($document_id, $feature_ids)
+
+Add an input document to Stupa server.
+
+$document_id parameter is the identifier of a document. $feature_ids parameter is the identifiers of the features of a document, and must be a array reference.
+
+=head2 delete_document($document_id)
+
+Delete a documen from Stupa server.
+
+$document_id parameter is the identifier of a document.
+
+=head2 size()
+
+Get the number of stored documents in Stupa server.
+
+=head2 search_by_document($document_ids, $max)
+
+Search documents by a query of document ids.
+
+$document_ids parameter is the identifiers of documents which are used as a search query, and must be array reference. $max parameter is the maximum number of search results.
+
+=head2 search_by_feature($feature_ids, $max)
+
+Search documents by queries of document ids.
+
+$feature_ids parameter is the identifiers of features which are used as a search query, and must be array reference. $max parameter is the maximum number of search results.
+
+=head2 save($filename)
+
+Save current state (documents, indexes) to a file.
+
+$filename parameter is the path of the saved file.
+
+=head2 load($filename)
+
+Load documents and indexes from a file.
+
+$filename parameter is the path of the loaded file.
 
 =head1 AUTHOR
 
